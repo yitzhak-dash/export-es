10d5bdccda0820e6e74f5aa053f83111a787e150
Yitzhak Dashevsky <idashevsky@gmail.com>
2018-07-29 16:12:53 +0300

    run from settings

--- main.py
+++ main.py
@@ -2,54 +2,57 @@ import os
 import csv
 from elasticsearch import Elasticsearch
 
-import settings
 import query_parser as parser
 
-es = Elasticsearch([settings.ES_HOST], port=9200, timeout=300)
-file_path = settings.OUTPUT
-body = settings.ES_QUERY
 
+class ExportES:
+    def __init__(self, settings):
+        self.settings = settings
 
-def write_data_to_csv(data):
-    with open(file_path, 'a', newline='') as newFile:
-        newFileWriter = csv.writer(newFile)
-        for row in data:
-            newFileWriter.writerow(parser.get_row(body, row))
+    def run(self):
+        self.es = Elasticsearch([self.settings.ES_HOST], port=9200, timeout=300)
 
+        self.file_path = self.settings.OUTPUT
+        self.body = parser.format(self.settings.ES_QUERY)
+        self.es_index = self.settings.ES_INDEX
+        self.size = self.settings.SCROLL_SIZE
 
-def silent_remove(filename):
-    try:
-        os.remove(filename)
-    except OSError:
-        pass
+        self.main()
 
+    def write_data_to_csv(self, data):
+        with open(self.file_path, 'a', newline='') as newFile:
+            newFileWriter = csv.writer(newFile)
+            for row in data:
+                newFileWriter.writerow(parser.get_row(self.body, row))
 
-def write_header_to_csv(filename, header):
-    with open(filename, 'a', newline='') as newFile:
-        newFileWriter = csv.writer(newFile)
-        newFileWriter.writerow(header)
+    def silent_remove(self, filename):
+        try:
+            os.remove(filename)
+        except OSError:
+            pass
 
+    def write_header_to_csv(self, filename, header):
+        with open(filename, 'a', newline='') as newFile:
+            newFileWriter = csv.writer(newFile)
+            newFileWriter.writerow(header)
 
-def main(size):
-    page = es.search(
-        index=settings.ES_INDEX,
-        scroll='2m',
-        size=size,
-        body=body,
-        request_timeout=30)
-    sid = page['_scroll_id']
-    scroll_size = page['hits']['total']
-    silent_remove(file_path)
-    write_header_to_csv(file_path, parser.get_column_names(settings.ES_QUERY))
-    while (scroll_size > 0):
-        print("Scrolling...")
-        page = es.scroll(scroll_id=sid, scroll='2m')
+    def main(self):
+        page = self.es.search(
+            index=self.es_index,
+            scroll='2m',
+            size=self.size,
+            body=self.body,
+            request_timeout=30)
         sid = page['_scroll_id']
-        scroll_size = len(page['hits']['hits'])
-        print("scroll size: " + str(scroll_size))
-        write_data_to_csv(page['hits']['hits'])
-    print("************** completed **************")
+        scroll_size = page['hits']['total']
+        self.silent_remove(self.file_path)
+        self.write_header_to_csv(self.file_path, parser.get_column_names(self.body))
+        while (scroll_size > 0):
+            print("Scrolling...")
+            page = self.es.scroll(scroll_id=sid, scroll='2m')
+            sid = page['_scroll_id']
+            scroll_size = len(page['hits']['hits'])
+            print("scroll size: " + str(scroll_size))
+            self.write_data_to_csv(page['hits']['hits'])
+        print("************** completed **************")
 
-
-if __name__ == '__main__':
-    main(settings.SCROLL_SIZE)
--- settings.py
+++ settings.py
@@ -1,156 +1,167 @@
-ES_HOST = "es1.teridion.net"
+from main import ExportES
 
-ES_INDEX = "tcrtrafficagg"
 
-OUTPUT = "./output/data.csv"
+class Config:
+    ### CONFIGURATION
 
-SCROLL_SIZE = 1000
+    ES_HOST = "es1.teridion.net"
 
-ES_QUERY = '''
-{
-    "query": {
-        "bool": {
-            "filter": [
-                {
-                    "bool": {
-                        "must": [
-                            {
-                                "bool": {
-                                    "must": [
-                                        {
-                                            "range": {
-                                                "timestamp": {
-                                                    "from": "2018-05-01",
-                                                    "to": None,
-                                                    "include_lower": True,
-                                                    "include_upper": True,
-                                                    "boost": 1
+    ES_INDEX = "tcrtrafficagg"
+
+    OUTPUT = "./output/data.csv"
+
+    SCROLL_SIZE = 1000
+
+    ES_QUERY = '''
+    {
+        "query": {
+            "bool": {
+                "filter": [
+                    {
+                        "bool": {
+                            "must": [
+                                {
+                                    "bool": {
+                                        "must": [
+                                            {
+                                                "range": {
+                                                    "timestamp": {
+                                                        "from": "2018-05-01",
+                                                        "to": null,
+                                                        "include_lower": true,
+                                                        "include_upper": true,
+                                                        "boost": 1
+                                                    }
                                                 }
-                                            }
-                                        },
-                                        {
-                                            "range": {
-                                                "timestamp": {
-
-                                                    "from": None,
-
-                                                    "to": "2018-05-15",
-
-                                                    "include_lower": True,
-
-                                                    "include_upper": True,
-
-                                                    "boost": 1
-
+                                            },
+                                            {
+                                                "range": {
+                                                    "timestamp": {
+    
+                                                        "from": null,
+    
+                                                        "to": "2018-05-15",
+    
+                                                        "include_lower": true,
+    
+                                                        "include_upper": true,
+    
+                                                        "boost": 1
+    
+                                                    }
+    
                                                 }
-
+    
                                             }
-
-                                        }
-
-                                    ],
-
-                                    "disable_coord": False,
-
-                                    "adjust_pure_negative": True,
-
-                                    "boost": 1
-
+    
+                                        ],
+    
+                                        "disable_coord": false,
+    
+                                        "adjust_pure_negative": true,
+    
+                                        "boost": 1
+    
+                                    }
+    
                                 }
-
-                            }
-
-                        ],
-
-                        "disable_coord": False,
-
-                        "adjust_pure_negative": True,
-
-                        "boost": 1
-
+    
+                            ],
+    
+                            "disable_coord": false,
+    
+                            "adjust_pure_negative": true,
+    
+                            "boost": 1
+    
+                        }
+    
                     }
-
-                }
-
+    
+                ],
+    
+                "disable_coord": false,
+    
+                "adjust_pure_negative": true,
+    
+                "boost": 1
+    
+            }
+    
+        },
+    
+        "_source": {
+    
+            "includes": [
+    
+                "networkName",
+    
+                "sourceTcrId",
+    
+                "timestamp",
+    
+                "sourceDatacenter"
+    
             ],
-
-            "disable_coord": False,
-
-            "adjust_pure_negative": True,
-
-            "boost": 1
-
-        }
-
-    },
-
-    "_source": {
-
-        "includes": [
-
-            "networkName",
-
-            "sourceTcrId",
-
-            "timestamp",
-
-            "sourceDatacenter"
-
-        ],
-
-        "excludes": []
-
-    },
-
-    "script_fields": {
-
-        "upload1": {
-
-            "script": {
-
-                "inline": "doc['clientToServer.incomingBytes'].value",
-
-                "lang": "painless"
-
-            },
-
-            "ignore_failure": False
-
+    
+            "excludes": []
+    
         },
-
-        "download1": {
-
-            "script": {
-
-                "inline": "doc['serverToClient.outgoingBytes'].value",
-
-                "lang": "painless"
-
+    
+        "script_fields": {
+    
+            "upload1": {
+    
+                "script": {
+    
+                    "inline": "doc['clientToServer.incomingBytes'].value",
+    
+                    "lang": "painless"
+    
+                },
+    
+                "ignore_failure": false
+    
             },
-
-            "ignore_failure": False
-
-        },
-
-        "upload2": {
-
-            "script": {
-
-                "inline": "doc['serverToClient.incomingBytes'].value",
-
-                "lang": "painless"
-
+    
+            "download1": {
+    
+                "script": {
+    
+                    "inline": "doc['serverToClient.outgoingBytes'].value",
+    
+                    "lang": "painless"
+    
+                },
+    
+                "ignore_failure": false
+    
             },
-            "ignore_failure": False
-        },
-        "download2": {
-            "script": {
-                "inline": "doc['clientToServer.outgoingBytes'].value",
-                "lang": "painless"
+    
+            "upload2": {
+    
+                "script": {
+    
+                    "inline": "doc['serverToClient.incomingBytes'].value",
+    
+                    "lang": "painless"
+    
+                },
+                "ignore_failure": false
             },
-            "ignore_failure": False
+            "download2": {
+                "script": {
+                    "inline": "doc['clientToServer.outgoingBytes'].value",
+                    "lang": "painless"
+                },
+                "ignore_failure": false
+            }
         }
     }
-}
-'''
+    '''
 
+    ### END CONFIGURATION
+
+
+if __name__ == '__main__':
+    ExportES(Config()).run()
